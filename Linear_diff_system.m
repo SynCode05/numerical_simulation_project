36b69694840baabc8a9adcec31f6f26c1641bae2
Syn <45761547+SynCode05@users.noreply.github.com>
2025-05-04 16:52:15 +0200

    Deliverables

--- Linear_diff_system.m
+++ Linear_diff_system.m
@@ -37,10 +37,33 @@ plot(t, euler_numerical_solution(1, :), 'r-o');
 plot(t, euler_numerical_solution(2, :), 'g-o');
 plot(t, euler_numerical_solution(3, :), 'b-o');
 
+% Plot RK45 method numerical solutions
+plot(t, RK45_numerical_solution(1, :), 'r-x');
+plot(t, RK45_numerical_solution(2, :), 'g-x');
+plot(t, RK45_numerical_solution(3, :), 'b-x');
+
+
 % Plot analyitical solutions
-plot(t, analytical_solution(1, :), 'r-x');
-plot(t, analytical_solution(2, :), 'g-x');
-plot(t, analytical_solution(3, :), 'b-x');
+plot(t, analytical_solution(1, :), 'r--');
+plot(t, analytical_solution(2, :), 'g--');
+plot(t, analytical_solution(3, :), 'b--');
+
+hold off;
+
+legend('x(t) Euler Numerical', 'y(t) Euler Numerical', 'z(t) Euler Numerical', 'x(t) RK45 Numerical', 'y(t) RK45 Numerical', 'z(t) RK45 Numerical', 'x(t) Analytical', 'y(t) Analytical', 'z(t) Analytical', 'Location', 'northwest')
+xlabel('Time t')
+ylabel('Solution value')
+title('Solution of Linear System')
+grid on;
+
+figure(2);
+
+hold on;
+
+% Plot absolute error margins
+plot(t, abs(analytical_solution(1,:) - RK45_numerical_solution(1,:)), 'r-x', 'LineWidth', 2);
+plot(t, abs(analytical_solution(2,:) - RK45_numerical_solution(2,:)), 'g-x', 'LineWidth', 2);
+plot(t, abs(analytical_solution(3,:) - RK45_numerical_solution(3,:)), 'b-x', 'LineWidth', 2);
 
 % Plot error margins
 plot(t, abs(analytical_solution(1,:) - euler_numerical_solution(1,:)), 'r--', 'LineWidth', 2);
@@ -49,60 +72,8 @@ plot(t, abs(analytical_solution(3,:) - euler_numerical_solution(3,:)), 'b--', 'L
 
 hold off;
 
-legend('x(t) Euler Numerical', 'y(t) Euler Numerical', 'z(t) Euler Numerical', 'x(t) Analytical', 'y(t) Analytical', 'z(t) Analytical', 'x(t) Error', 'y(t) Error', 'z(t) Error', 'Location', 'northwest')
+legend('x(t) RK45 Abs. Error', 'y(t) RK45 Abs. Error', 'z(t) RK45 Abs. Error', 'x(t) Euler Abs. Error', 'y(t) Euler Abs. Error', 'z(t) Euler Abs. Error', 'Location', 'northwest')
 xlabel('Time t')
-ylabel('Solution value')
-title('Analytical Solution of Linear System (Euler Method)')
-grid on;
-
-figure(2);
-
-hold on;
-% Plot RK45 method numerical solutions
-plot(t, RK45_numerical_solution(1, :), 'r-o');
-plot(t, RK45_numerical_solution(2, :), 'g-o');
-plot(t, RK45_numerical_solution(3, :), 'b-o');
-
-% Plot analyitical solutions
-plot(t, analytical_solution(1, :), 'r-x');
-plot(t, analytical_solution(2, :), 'g-x');
-plot(t, analytical_solution(3, :), 'b-x');
-
-% Plot error margins
-plot(t, abs(analytical_solution(1,:) - RK45_numerical_solution(1,:)), 'r--', 'LineWidth', 2);
-plot(t, abs(analytical_solution(2,:) - RK45_numerical_solution(2,:)), 'g--', 'LineWidth', 2);
-plot(t, abs(analytical_solution(3,:) - RK45_numerical_solution(3,:)), 'b--', 'LineWidth', 2);
-
-hold off;
-
-legend('x(t) Numerical RK45', 'y(t) Numerical RK45', 'z(t) Numerical RK45', 'x(t) Analytical', 'y(t) Analytical', 'z(t) Analytical', 'x(t) Error', 'y(t) Error', 'z(t) Error', 'Location', 'northwest')
-xlabel('Time t')
-ylabel('Solution value')
-title('Analytical Solution of Linear System (RK45 Method)')
-grid on;
-
-figure(3);
-
-hold on;
-% Plot RK45 method numerical solutions
-plot(t, RK45_numerical_solution(1, :), 'r-o');
-plot(t, RK45_numerical_solution(2, :), 'g-o');
-plot(t, RK45_numerical_solution(3, :), 'b-o');
-
-% Plot euler method numerical solutions
-plot(t, euler_numerical_solution(1, :), 'r-x');
-plot(t, euler_numerical_solution(2, :), 'g-x');
-plot(t, euler_numerical_solution(3, :), 'b-x');
-
-% Plot error margins
-plot(t, abs(euler_numerical_solution(1,:) - RK45_numerical_solution(1,:)), 'r--', 'LineWidth', 2);
-plot(t, abs(euler_numerical_solution(2,:) - RK45_numerical_solution(2,:)), 'g--', 'LineWidth', 2);
-plot(t, abs(euler_numerical_solution(3,:) - RK45_numerical_solution(3,:)), 'b--', 'LineWidth', 2);
-
-hold off;
-
-legend('x(t) Numerical RK45', 'y(t) Numerical RK45', 'z(t) Numerical RK45', 'x(t) Numerical Euler', 'y(t) Numerical Euler', 'z(t) Numerical Euler', 'x(t) Error', 'y(t) Error', 'z(t) Error', 'Location', 'northwest')
-xlabel('Time t')
-ylabel('Solution value')
-title('Analytical Solution of Linear System (RK45 vs Euler Method)')
+ylabel('Absolute Error')
+title('Absolution Errors of the Solutions of the Sinear System ')
 grid on;
--- Nonlinear_diff_system.m
+++ Nonlinear_diff_system.m
@@ -25,26 +25,39 @@ wrapped_differential_system = @(t, Y) differential_system(Y(1), Y(2), Y(3));
 [t, euler_numerical_solution] = Euler_Method(differential_system, 0, 5, 0.1, initial_conditions);
 [t, RK45_numerical_solution] = Classical_Runge_Kutta(wrapped_differential_system, 0, 5, 0.1, initial_conditions);
 
+figure(1);
+
 hold on;
-% Plot RK45 method numerical solutions
-plot(t, RK45_numerical_solution(1, :), 'r-o');
-plot(t, RK45_numerical_solution(2, :), 'g-o');
-plot(t, RK45_numerical_solution(3, :), 'b-o');
-
 % Plot euler method numerical solutions
-plot(t, euler_numerical_solution(1, :), 'r--');
-plot(t, euler_numerical_solution(2, :), 'g--');
-plot(t, euler_numerical_solution(3, :), 'b--');
+plot(t, euler_numerical_solution(1, :), 'r-o');
+plot(t, euler_numerical_solution(2, :), 'g-o');
+plot(t, euler_numerical_solution(3, :), 'b-o');
 
-% Plot error margins
-plot(t, abs(euler_numerical_solution(1,:) - RK45_numerical_solution(1,:)), 'r--', 'LineWidth', 2);
-plot(t, abs(euler_numerical_solution(2,:) - RK45_numerical_solution(2,:)), 'g--', 'LineWidth', 2);
-plot(t, abs(euler_numerical_solution(3,:) - RK45_numerical_solution(3,:)), 'b--', 'LineWidth', 2);
+% Plot RK45 method numerical solutions
+plot(t, RK45_numerical_solution(1, :), 'r-x');
+plot(t, RK45_numerical_solution(2, :), 'g-x');
+plot(t, RK45_numerical_solution(3, :), 'b-x');
+hold off;
+
+legend('x(t) Euler Numerical', 'y(t) Euler Numerical', 'z(t) Euler Numerical', 'x(t) RK45 Numerical', 'y(t) RK45 Numerical', 'z(t) RK45 Numerical', 'Location', 'northwest')
+xlabel('Time t')
+ylabel('Solution value')
+title('Numerical solution of Linear System')
+grid on;
+
+figure(2);
+
+hold on;
+
+% Plot absolute error margins
+plot(t, abs(RK45_numerical_solution(1,:) - euler_numerical_solution(1,:)), 'r--', 'LineWidth', 2);
+plot(t, abs(RK45_numerical_solution(2,:) - euler_numerical_solution(2,:)), 'g--', 'LineWidth', 2);
+plot(t, abs(RK45_numerical_solution(3,:) - euler_numerical_solution(3,:)), 'b--', 'LineWidth', 2);
 
 hold off;
 
-legend('x(t) Numerical RK45', 'y(t) Numerical RK45', 'z(t) Numerical RK45', 'x(t) Numerical Euler', 'y(t) Numerical Euler', 'z(t) Numerical Euler', 'x(t) Error', 'y(t) Error', 'z(t) Error', 'Location', 'southwest')
-xlabel('Time t');
-ylabel('Solution value');
-title('Numerical Solution of Nonlinear System (RK45 vs Euler Method)');
+legend('x(t) Abs. Error', 'y(t) Abs. Error', 'z(t) Abs. Error', 'Location', 'northwest')
+xlabel('Time t')
+ylabel('Solution value')
+title('Absolute error of numerical solutions')
 grid on;
